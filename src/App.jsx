355faceddc49d71f8c3f65482ff7dc3ab06c9315
Asif <masifmirza927@gmail.com>
2024-11-12 10:24:39 +0500

    state with fetch api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
 import Child from "./Child"
-
-import {useState} from "react";
+import { useState } from "react";
 import Scorecard from "./Scorecard";
 
 function App() {
-    const [score, setScore] = useState(0);
-console.log("re run");
+    const [products, setProducts] = useState([]);
 
+    const getProducts = () => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }
 
-    return ( 
-        <div>
-           <h1>Score {score}</h1>
-            <button onClick={ () => { setScore(6) } } >Six</button>
-            
-            {/* <div><button onClick={ () => { getCity("Karachi") }  } >Get City Btn</button></div> */}
-            {/* <Child getCity={getCity} name={name} /> */}
-            <Scorecard score={score} />
+    console.log(products);
+    return (
+        <div className="container">
+            <button className="btn btn-warning" onClick={getProducts}>Get All Products</button>
+            <div className="row">
+                {
+                    products.map((product) => {
+                        return (
+                            <div className="col-md-4" key={product.id}>
+                                <div className="card">
+                                    <img src={product.image} className="card-img-top object-fit-cover" alt="..." style={{height: "250px"}} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{product.title}</h5>
+                                            <p className="card-text">{product.description}</p>
+                                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                                        </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }
